917a0f58c5e50517afd5484f17dd0fc91a163985
Leotrim Hamiti <leotrimh12@gmail.com>
2026-03-01 22:02:55 +0100

    Mobile App toggle fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,18 @@ import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../contexts/AuthContext.jsx";
 import "../css/Navbar.css"
 import profilepic from "../assets/profile-pic.png";
-
+import burgerMenu from "../assets/ham-menu.png";
+import { useState } from "react";
 function Navbar() {
+
+    const [burger, setBurger] = useState(false)
+
+    const handleBurger = () => {
+        setBurger(!burger);
+    }
+
+    const closeMenu = () => setBurger(false);
+
     const { user, logout } = useAuth();
     const navigate = useNavigate();
 
@@ -33,8 +43,28 @@ function Navbar() {
                         <Link to='/signup' className="nav-link">Sign up</Link>
                         <Link to='/login' className="nav-link">Log in</Link>
                     </>
-                )}
+                )}    
             </div>
+            <img className="burger-menu" src={burgerMenu} onClick={handleBurger}></img>
+            <div className={`navbar-links-burger ${burger ? "open" : ""}`}>
+                <Link to='/' className="nav-link" onClick={closeMenu}>Home</Link>
+                <Link to='/favourites' className="nav-link" onClick={closeMenu}>Favourites</Link>
+                
+                {user ? (
+                    <>  <div className="profile-picture-logout" onClick={closeMenu}>
+                        <span className="user-greet"><img className="profile-pic" src={profilepic}></img>{user.username}</span>
+                        <button onClick={handleLogout} className="logout-btn">Log Out</button></div>
+                    
+                        
+                    </>
+                ) : (
+                    <>
+                        <Link to='/signup' className="nav-link" onClick={closeMenu}>Sign up</Link>
+                        <Link to='/login' className="nav-link" onClick={closeMenu}>Log in</Link>
+                    </>
+                )}    
+            </div>
+
         </nav>
     )
 }
